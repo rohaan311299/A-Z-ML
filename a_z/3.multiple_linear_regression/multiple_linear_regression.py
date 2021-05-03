04a424a3a726b4f05faabaa9bb8b742a2c984c4a
Rohan Kacheria <rohankacheriam@gmail.com>
2021-05-03 23:05:03 +0530

    Multiple Linear Regression

---
+++ a_z/3.multiple_linear_regression/multiple_linear_regression.py
@@ -0,0 +1,32 @@
+# Importing the libraries
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+# Importing the dataset
+dataset=pd.read_csv("50_Startups.csv")
+X=dataset.iloc[:,:-1].values
+y=dataset.iloc[:,-1].values
+# print(X)
+
+# Encoding categorical data
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[("encoder", OneHotEncoder(), [3])], remainder="passthrough")
+X = np.array(ct.fit_transform(X))
+# print(X)
+
+# Splitting the dataset into training and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Training the multiple linear regression model on the training set
+from sklearn.linear_model import LinearRegression
+regressor=LinearRegression()
+regressor.fit(X_train,y_train)
+
+# Predicting the test set results
+y_pred=regressor.predict(X_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+ 
