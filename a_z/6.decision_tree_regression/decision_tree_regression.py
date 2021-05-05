3880072759c0bc4e3b53efb2a97bd1d973db1c76
Rohan Kacheria <rohankacheriam@gmail.com>
2021-05-05 10:59:23 +0530

    Decision Tree Regression

---
+++ a_z/6.decision_tree_regression/decision_tree_regression.py
@@ -0,0 +1,28 @@
+# Importing the libraries
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt 
+
+# Importing the dataset
+dataset=pd.read_csv("./Position_Salaries.csv")
+X=dataset.iloc[:,1:-1].values
+y=dataset.iloc[:,-1].values
+
+# Training the decision tree regression model on the whole dataset
+from sklearn.tree import DecisionTreeRegressor
+regressor=DecisionTreeRegressor(random_state=0)
+regressor.fit(X,y)
+
+# Predicting a new result
+prediction=regressor.predict([[6.5]])
+# print(prediction)
+
+# Visualising the decison tre regression results
+X_grid=np.arange(min(X),max(X),0.01)
+X_grid=X_grid.reshape((len(X_grid),1))
+plt.scatter(X,y,color="red")
+plt.plot(X_grid,regressor.predict(X_grid),color="blue")
+plt.title("Truth or Bluff ( Decision Tree Regression)")
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
